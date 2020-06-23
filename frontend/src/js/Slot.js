f5977f36b81435aacafb735b97f93b5223944fe0
Santiago Mena <santiagomenape@gmail.com>
2020-06-23 08:43:39 -0300

    Bonus

--- frontend/src/js/Slot.js
+++ frontend/src/js/Slot.js
@@ -81,12 +81,17 @@ export default class Slot {
   onSpinEnd({ganancias, gananciaTotal, juegosExtra}) {
     this.spinButton.disabled = false;
 
-    if( typeof juegosExtra !== 'undefined' ) {
+    this.imprimirGanancias({ganancias});
+
+    if( typeof juegosExtra !== 'undefined' && juegosExtra.length > 0 ) {
+
       alert(`Bonus! Tiene ${ juegosExtra.length } juegos extra!`);
+      return window.setTimeout(() => this.spinJuegosExtra({juegosExtra, gananciaTotal}), 200);
+
+    } else {
+      alert(`Ganancia! Tiene $${ gananciaTotal } de ganancia total!`);
     }
 
-    this.imprimirGanancias({ganancias});
-    // alert(`Ganancia! Tiene ${ gananciaTotal } de ganancia total!`);
 
     console.log("SPIN END");
 
@@ -94,7 +99,31 @@ export default class Slot {
       return window.setTimeout(() => this.spin(), 200);
   }
 
-  spinJuegosExtra({juegosExtra}) {
+  spinJuegosExtra({juegosExtra, gananciaTotal}) {
+    this.onSpinStart();
+
+    this.currentSymbols = this.nextSymbols;
+
+    let { resultados } = juegosExtra.shift();
+    
+    let { tablero, ganancias } = resultados;
+    let { elementos } = tablero;
+
+    this.nextSymbols = [
+      [elementos[0][0].imagen, elementos[1][0].imagen, elementos[2][0].imagen],
+      [elementos[0][1].imagen, elementos[1][1].imagen, elementos[2][1].imagen],
+      [elementos[0][2].imagen, elementos[1][2].imagen, elementos[2][2].imagen],
+      [elementos[0][3].imagen, elementos[1][3].imagen, elementos[2][3].imagen],
+      [elementos[0][4].imagen, elementos[1][4].imagen, elementos[2][4].imagen],
+    ];
+
+    return Promise.all(
+      this.reels.map((reel) => {
+        reel.renderSymbols(this.nextSymbols[reel.idx]);
+        return reel.spin();
+      })
+    ).then(() => this.onSpinEnd({ganancias, gananciaTotal, juegosExtra}));
+
 
   }
 
